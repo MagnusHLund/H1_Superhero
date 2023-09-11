61a9e49de0524329b8b20d4c109c4a872d3342cd
MagnusHLund <magnusherringelund@gmail.dk>
2023-09-11 11:57:04 +0200

    Add project files.

---
+++ H1_Superhero/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+
+namespace H1_Superhero
+{
+    internal class Program
+    {
+        // Creates an array with 3 positions
+        Superhero[] hero = new Superhero[3];
+
+        static void Main()
+        {
+            // Calls the superheroes method, which is not static
+            Program program = new Program();
+            program.Superheroes();
+        }
+
+        void Superheroes()
+        {
+            // Assigns a new class "Superhero" to each of the positions in the hero array
+            for(int i = 0; i < 3; i++)
+            {
+                hero[i] = new Superhero();
+            }
+            
+            // Assigns values to each of the super heroes, by calling the properties
+            hero[0].ActualName = "Bruce Wayne";
+            hero[0].HeroName = "Batman";
+            hero[0].Powers = "none";
+            hero[0].HasSecretIdentity = true;
+            hero[0].WearsCostume = true;
+            hero[0].FightsAlone = false;
+            hero[0].HasStrongMoralComplex = true;
+            hero[0].HasNemesis = true;
+            hero[0].HasPrivateAnxiety = false;
+
+            hero[1].ActualName = "Peter Parker";
+            hero[1].HeroName = "Spider-man";
+            hero[1].Powers = "Spider-sense, biological webshooters"; // Tobey maguire's spider-man
+            hero[1].HasSecretIdentity = true;
+            hero[1].WearsCostume = true;
+            hero[1].FightsAlone = true;
+            hero[1].HasStrongMoralComplex = true;
+            hero[1].HasNemesis = false;
+            hero[1].HasPrivateAnxiety = true;
+
+            hero[2].ActualName = "Diana Prince";
+            hero[2].HeroName = "Wonder-Woman";
+            hero[2].Powers = "Strong";
+            hero[2].HasSecretIdentity = true;
+            hero[2].WearsCostume = true;
+            hero[2].FightsAlone = false;
+            hero[2].HasStrongMoralComplex = false;
+            hero[2].HasNemesis = false;
+            hero[2].HasPrivateAnxiety = false;
+
+            view();
+        }
+
+        void view()
+        {
+            // For loop to go through each hero in the hero array
+            for (int i = 0; i < 3; i++)
+            {
+                // Checks each property for the hero in the array
+                foreach (PropertyInfo info in hero[i].GetType().GetProperties())
+                {
+                    // Outputs the name of the property, as well as the value
+                    Console.WriteLine($"{info.Name}: {info.GetValue(hero[i])}");
+                }
+
+                // Creates a space
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ H1_Superhero/Superhero.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Superhero
+{
+    /// <summary>
+    /// The superhero class contains a bunch of properties, which contain the data about our super heroes
+    /// </summary>
+    internal class Superhero
+    {
+        private string _actualName = "";
+        private string _heroName = "";
+        private string _powers = "";
+        private bool _hasSecretIdentity;
+        private bool _wearsCostume;
+        private bool _fightsAlone;
+        private bool _hasStrongMoralComplex;
+        private bool _hasNemesis;
+        private bool _hasPrivateAnxiety;
+
+        public string ActualName
+        {
+            get { return _actualName; }
+            set { _actualName = value; }
+        }
+
+        public string HeroName 
+        { 
+            get { return _heroName; } 
+            set { _heroName = value; }
+        }
+
+        public string Powers
+        {
+            get { return _powers; }
+            set { _powers = value; }
+        }
+
+        public bool HasSecretIdentity
+        {
+            get { return _hasSecretIdentity; }
+            set { _hasSecretIdentity = value; }
+        }
+
+        public bool WearsCostume
+        {
+            get { return _wearsCostume; }
+            set { _wearsCostume = value; }
+        }
+
+        public bool FightsAlone
+        {
+            get { return _fightsAlone; }
+            set { _fightsAlone = value; }
+        }
+
+        public bool HasStrongMoralComplex
+        {
+            get { return _hasStrongMoralComplex; }
+            set { _hasStrongMoralComplex = value; }
+        }
+
+        public bool HasNemesis
+        {
+            get { return _hasNemesis; }
+            set { _hasNemesis = value; }
+        }
+
+        public bool HasPrivateAnxiety
+        {
+            get { return _hasPrivateAnxiety; }
+            set { _hasPrivateAnxiety = value; }
+        }
+    }
+}
